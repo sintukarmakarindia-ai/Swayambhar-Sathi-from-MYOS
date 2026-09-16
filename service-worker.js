de47726139d1ebe5c0208b21fd031d386b400dc6
sintukarmakarindia-ai <sintukarmakar.india@gmail.com>
2026-09-16 17:15:50 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,97 @@
+const CACHE_NAME = "swayambhar-sathi-pwa-v1";
+
+const APP_FILES = [
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./splash.jpg",
+    "./logoleft.jpg",
+    "./logomiddle.jpg",
+    "./logoright.jpg",
+    "./icon-192.jpg",
+    "./icon-512.jpg"
+];
+
+
+/* =========================
+   INSTALL
+========================= */
+
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME).then(cache => {
+
+            return cache.addAll(APP_FILES);
+
+        })
+
+    );
+
+    self.skipWaiting();
+
+});
+
+
+/* =========================
+   ACTIVATE
+========================= */
+
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+
+        caches.keys().then(cacheNames => {
+
+            return Promise.all(
+
+                cacheNames.map(cacheName => {
+
+                    if (cacheName !== CACHE_NAME) {
+
+                        return caches.delete(cacheName);
+
+                    }
+
+                })
+
+            );
+
+        })
+
+    );
+
+    self.clients.claim();
+
+});
+
+
+/* =========================
+   FETCH
+========================= */
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request).then(cachedResponse => {
+
+            if (cachedResponse) {
+
+                return cachedResponse;
+
+            }
+
+
+            return fetch(event.request).catch(() => {
+
+                return caches.match("./index.html");
+
+            });
+
+        })
+
+    );
+
+});
